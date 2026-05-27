b0c506027d434ca7ae5f03c9f2e7e907955c0ca9
Fabio5271 <89141452+Fabio5271@users.noreply.github.com>
2026-05-27 16:33:38 -0300

    Base frontend structure, layout, assets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'about',
+        loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent)
+    },
+    {
+        path: 'graphs',
+        loadComponent: () => import('./pages/graphs/graphs.component').then(m => m.GraphsComponent)
+    },
+    {
+        path: 'xgboost',
+        loadComponent: () => import('./pages/xgboost/xgboost.component').then(m => m.XgboostComponent)
+    },
+    {
+        path: '**',
+        redirectTo: 'about'
+    }
+];
---
+++ src/app/pages/xgboost/xgboost.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-xgboost',
+  imports: [],
+  templateUrl: './xgboost.component.html',
+  styleUrl: './xgboost.component.css'
+})
+export class XgboostComponent {
+
+}
